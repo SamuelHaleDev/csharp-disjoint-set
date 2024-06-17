d55e2fa72ec298fc0ec421dcb5fe8ac4e6121772
SamuelHaleDev <samuelhalebusiness@gmail.com>
2024-06-17 09:15:06 -0400

    Implemented DisjointSets interface

--- Program.cs
+++ Program.cs
@@ -1,4 +1,16 @@
-﻿namespace csharp_disjoint_set;
+﻿using System.Data;
+
+namespace csharp_disjoint_set;
+
+public interface DisjointSets {
+    // Connects items p and q
+    public void Connect(int p, int q);
+
+    // Checks to see if two items are connected
+    public void IsConnected(int p, int q);
+}
+
+
 
 class Program
 {
